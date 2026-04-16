f7bb9f6c3b17af207c1d92f2023f38e9f424c4b0
IntegratedQuantum <jahe788@gmail.com>
2026-04-16 17:07:11 +0200

    Update to 0.16.0 and juicy main

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,6 @@ const std = @import("std");
 const process = std.process;
 const fmt = @import("zig fmt");
 
-pub fn main() !void {
-	var gpa = std.heap.DebugAllocator(.{}).init;
-	var arena = std.heap.ArenaAllocator.init(gpa.allocator());
-	defer arena.deinit();
-	const args = try process.argsAlloc(arena.allocator());
-	var threadedIo = std.Io.Threaded.init(gpa.allocator());
-	defer threadedIo.deinit();
-
-	try fmt.run(gpa.allocator(), arena.allocator(), threadedIo.io(), args[1..]);
+pub fn main(init: std.process.Init) !void {
+	try fmt.run(init.gpa, init.arena.allocator(), init.io, (try init.minimal.args.toSlice(init.arena.allocator()))[1..]);
 }
